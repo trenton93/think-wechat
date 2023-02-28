c2f984fead05e9162f9241ef61e089b728d30a91
Trenton <260053530@qq.com>
2023-02-28 19:26:14 +0800

    init commit

---
+++ src/CacheBridge.php
@@ -0,0 +1,62 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkWechat 缓存类
+// +----------------------------------------------------------------------
+// | Copyright (c) 2023 https://leapfu.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: Trenton <370887237@qq.com>
+// +----------------------------------------------------------------------
+
+namespace trenton93\ThinkWechat;
+
+use Psr\SimpleCache\CacheInterface;
+
+class CacheBridge implements CacheInterface
+{
+    protected $cache = null;
+
+    public function __construct()
+    {
+        $this->cache = app('cache');
+    }
+
+    public function get($key, $default = null)
+    {
+        return $this->cache->get($key, $default);
+    }
+
+    public function set($key, $value, $ttl = null)
+    {
+        return $this->cache->set($key, $value, $ttl);
+    }
+
+    public function delete($key)
+    {
+        return $this->cache->rm($key);
+    }
+
+    public function clear()
+    {
+        return $this->cache->clear();
+    }
+
+    public function getMultiple($keys, $default = null)
+    {
+    }
+
+    public function setMultiple($values, $ttl = null)
+    {
+    }
+
+    public function deleteMultiple($keys)
+    {
+    }
+
+    public function has($key)
+    {
+        return !is_null($this->cache->get($key));
+    }
+
+}
